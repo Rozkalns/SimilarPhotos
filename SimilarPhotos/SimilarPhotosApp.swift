2ece794efa54d2281c423b963008433fc5377b16
Roberts Rožkalns <roberts.rozkalns@me.com>
2026-05-30 11:50:21 +0300

    Initial Commit

---
+++ SimilarPhotos/SimilarPhotosApp.swift
@@ -0,0 +1,17 @@
+//
+//  SimilarPhotosApp.swift
+//  SimilarPhotos
+//
+//  Created by Roberts Rožkalns on 30/05/2026.
+//
+
+import SwiftUI
+
+@main
+struct SimilarPhotosApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
